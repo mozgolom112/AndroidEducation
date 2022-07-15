184fbdf46ee6df3d0a844796304c694a41929ca6
mozgolom112 <cell-26@yandex.ru>
2022-07-15 17:13:28 +0300

    Init new project

--- Lesson 2/AboutMe/app/src/main/java/com/mozgolom112/aboutme/MainActivity.kt
+++ Lesson 2/AboutMe/app/src/main/java/com/mozgolom112/aboutme/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.mozgolom112.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.mozgolom112.aboutme.databinding.ActivityMainBinding
 
@@ -36,6 +33,4 @@ class MainActivity : AppCompatActivity() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
-
-
 }
