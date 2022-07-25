4e6975f7395ea41e14e8c84a20b57528a923e769
mozgolom112 <cell-26@yandex.ru>
2022-07-25 12:56:13 +0300

    Add ScoreViewModelFactory

--- Lesson 5/Guess it/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ Lesson 5/Guess it/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -17,11 +17,14 @@
 package com.example.android.guesstheword.screens.score
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -32,19 +35,57 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  */
 class ScoreFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    private val viewModelFactory: ScoreViewModelFactory
+            by lazy {
+                Log.i("ScoreFragment", "viewModelFactory by lazy called")
+                ScoreViewModelFactory(navArgs<ScoreFragmentArgs>().value.score)
+            }
+    private val viewModel: ScoreViewModel
+            by lazy {
+                Log.i("ScoreFragment", "viewModel by lazy called")
+                ViewModelProvider(this, viewModelFactory).get(ScoreViewModel::class.java)
+            }
+    private lateinit var binding: ScoreFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.i("ScoreFragment", "onCreateView called")
         // Inflate view and obtain an instance of the binding class.
-        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(inflater,
-            R.layout.score_fragment, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.score_fragment, container, false
+        )
         // Get args using by navArgs property delegate
-        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.apply {
-            txtvScore.text = scoreFragmentArgs.score.toString()
-            btnPlayAgain.setOnClickListener { onPlayAgain() }
-        }
+        setClickListeners()
+        setObservers()
 
         return binding.root
+    }
+
+    private fun setClickListeners() {
+        binding.apply {
+            btnPlayAgain.setOnClickListener { viewModel.onPlayButton() }
+        }
+    }
+
+    private fun setObservers() {
+        viewModel.apply {
+            score.observe(this@ScoreFragment, Observer { newScore ->
+                Log.i("ScoreFragment", "Observe was called")
+                updateScoreText(newScore)
+            })
+            eventPlayAgain.observe(this@ScoreFragment, Observer { hasPlayAgain ->
+                if (hasPlayAgain) {
+                    onPlayAgain()
+                }
+            })
+        }
+    }
+
+    private fun updateScoreText(score: Int) {
+        binding.txtvScore.text = score.toString()
     }
 
     private fun onPlayAgain() {
