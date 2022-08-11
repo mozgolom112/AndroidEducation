ecf1bb149e05cceef9e634f538abc05b272fd7ba
mozgolom112 <cell-26@yandex.ru>
2022-08-11 14:47:50 +0300

    Сделан рефакторинг SleepNightAdapter

--- Lesson 7/Sleep Tracker v2 (RecycleView)/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ Lesson 7/Sleep Tracker v2 (RecycleView)/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -26,6 +26,8 @@ import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * These functions create a formatted string that can be set in a TextView.
@@ -47,6 +49,53 @@ fun convertNumericQualityToString(quality: Int, resources: Resources): String {
     return qualityString
 }
 
+/**
+ * These functions create a formatted string that can be set in a TextView.
+ */
+private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
+private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
+
+/**
+ * Convert a duration to a formatted string for display.
+ *
+ * Examples:
+ *
+ * 6 seconds on Wednesday
+ * 2 minutes on Monday
+ * 40 hours on Thursday
+ *
+ * @param startTimeMilli the start of the interval
+ * @param endTimeMilli the end of the interval
+ * @param res resources used to load formatted strings
+ */
+fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
+    val durationMilli = endTimeMilli - startTimeMilli
+    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
+    return when {
+        durationMilli < ONE_MINUTE_MILLIS -> {
+            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.seconds_length, seconds, weekdayString)
+        }
+        durationMilli < ONE_HOUR_MILLIS -> {
+            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.minutes_length, minutes, weekdayString)
+        }
+        else -> {
+            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.hours_length, hours, weekdayString)
+        }
+    }
+}
+
+fun getImgResByQuality(sleepQuality: Int) = when (sleepQuality) {
+    0 -> R.drawable.ic_sleep_0
+    1 -> R.drawable.ic_sleep_1
+    2 -> R.drawable.ic_sleep_2
+    3 -> R.drawable.ic_sleep_3
+    4 -> R.drawable.ic_sleep_4
+    5 -> R.drawable.ic_sleep_5
+    else -> R.drawable.ic_sleep_active
+}
 
 /**
  * Take the Long milliseconds returned by the system and stored in Room,
@@ -60,7 +109,7 @@ fun convertNumericQualityToString(quality: Int, resources: Resources): String {
 @SuppressLint("SimpleDateFormat")
 fun convertLongToDateString(systemTime: Long): String {
     return SimpleDateFormat("EEEE MMM-dd-yyyy' Time: 'HH:mm")
-            .format(systemTime).toString()
+        .format(systemTime).toString()
 }
 
 /**
@@ -106,4 +155,4 @@ fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
     }
 }
 
-class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
--- Lesson 7/Sleep Tracker v2 (RecycleView)/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Lesson 7/Sleep Tracker v2 (RecycleView)/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,44 +1,59 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
-import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.*
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     var data = listOf<SleepNight>()
         set(value) {
             //sledgehummer Не делай так
-            Log.i("SleepNightAdapter","SleepNightAdapter update data")
+            Log.i("SleepNightAdapter", "SleepNightAdapter update data")
             field = value
             notifyDataSetChanged()
         }
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        var color = Color.BLACK //Default
-        if (item.sleepQuality <= 1) color = Color.RED
+        holder.bind(item)
+    }
 
-        holder.textView.apply{
-            text = item.sleepQuality.toString()
-            setTextColor(color)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtvQuality: TextView = itemView.findViewById(R.id.txtvQualityString)
+        val txtvLenght: TextView = itemView.findViewById(R.id.txtvSleepLenght)
+        val imgvQuality: ImageView = itemView.findViewById(R.id.imgvQuality)
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
+                return ViewHolder(view)
+            }
         }
 
-        Log.i("SleepNightAdapter", "SleepNightAdapter onBindVH with item " + item.sleepQuality.toString())
+        fun bind(item: SleepNight) {
+            val res = itemView.context.resources
+            item.apply {
+                txtvLenght.text =
+                    convertDurationToFormatted(startTimeMilli, endTimeMilli, res)
+                txtvQuality.text = convertNumericQualityToString(sleepQuality, res)
+                imgvQuality.setImageResource(getImgResByQuality(sleepQuality))
+            }
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
-    }
 
 }
