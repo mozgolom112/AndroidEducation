b9e4e2a0be88b989778c27272b96f19fa5ee135f
mozgolom112 <cell-26@yandex.ru>
2022-09-07 20:01:29 +0300

    Пример упрощения adapter'a при использовании одного viewHolder'a

--- Lesson 8/MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/adapters/PhotoGridAdapter.kt
+++ Lesson 8/MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/overview/adapters/PhotoGridAdapter.kt
@@ -17,55 +17,33 @@
 
 package com.example.android.marsrealestate.overview.adapters
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ExpandableListView
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.example.android.marsrealestate.databinding.GridViewItemBinding
 import com.example.android.marsrealestate.network.MarsProperty
 import com.example.android.marsrealestate.overview.diffUtils.DiffCallback
 import com.example.android.marsrealestate.overview.viewHolders.MarsPropertyViewHolder
 
-private val ITEM_VIEW_TYPE_ITEM = 1
-
-
 class PhotoGridAdapter(private val clickListener: onClickListener) :
-    ListAdapter<MarsProperty, RecyclerView.ViewHolder>(DiffCallback) {
+    ListAdapter<MarsProperty, MarsPropertyViewHolder>(DiffCallback) {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPropertyViewHolder =
         getViewHolder(parent, viewType)
 
-    private fun getViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-        when (viewType) {
-            ITEM_VIEW_TYPE_ITEM -> MarsPropertyViewHolder.from(parent)
-            else -> throw ClassCastException("Unknown viewType $viewType")
-        }
+    private fun getViewHolder(parent: ViewGroup, viewType: Int): MarsPropertyViewHolder =
+        MarsPropertyViewHolder.from(parent)
 
-    override fun getItemViewType(position: Int): Int {
-        return when (getItem(position)) {
-            is MarsProperty -> ITEM_VIEW_TYPE_ITEM
-            else -> throw ClassCastException("Unknown viewType ${getItem(position)}")
-        }
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder) {
-            is MarsPropertyViewHolder -> {
-                val marsProperty = getItem(position)
-                holder.apply {
-                    bind(marsProperty)
-                    itemView.setOnClickListener{
-                        clickListener.onClick(marsProperty)
-                    }
-                }
+    override fun onBindViewHolder(holder: MarsPropertyViewHolder, position: Int) {
+        val marsProperty = getItem(position)
+        holder.apply {
+            bind(marsProperty)
+            itemView.setOnClickListener {
+                clickListener.onClick(marsProperty)
             }
         }
     }
 
-    class onClickListener(val clickListener: (MarsProperty)->Unit) {
+    class onClickListener(val clickListener: (MarsProperty) -> Unit) {
         fun onClick(marsProperty: MarsProperty) = clickListener(marsProperty)
     }
 }
